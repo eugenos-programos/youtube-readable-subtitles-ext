ec9305000253c79f377aef82a210df26bbbd207d
eugenos-programos <elena.samchuk.73@mail.ru>
2024-02-12 14:42:19 +0300

    feat: make model inference function

--- src/transformer.js
+++ src/transformer.js
@@ -1,8 +1,9 @@
 import { pipeline } from '@xenova/transformers';
 
 
-let pipe = await pipeline("token-classification", "ldenoue/fullstop-punctuation-multilang-large");
-let out = await pipe("hello world how are you");
-console.log(out);
-
-
+export async function call_normalization_model(subtitles)
+{
+    let pipe = await pipeline("token-classification", "ldenoue/fullstop-punctuation-multilang-large");
+    let out = await pipe(subtitles);
+    return out;
+}
