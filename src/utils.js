afb9017a0e2d252dd5ba2781140dd933badf320c
eugenos-programos <elena.samchuk.73@mail.ru>
2024-02-12 17:08:51 +0300

    fix: add spliting input text on chunks

--- src/utils.js
+++ src/utils.js
@@ -1,40 +1,41 @@
+import { call_normalization_model } from "./transformer.js";
 
 
-async function query(data) {
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/unikei/distilbert-base-re-punctuate",
-		{
-			headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.json();
-	return result;
+const inputTextLength = 1500;
+
+export async function normalize_subtitles(subtitles) {
+    subtitles = clean_subtitles(subtitles);
+    let result = '';
+    do
+    {
+        let last_space_index = extractLastSpaceIndex(subtitles);
+        let truncated_text = subtitles.substring(0, last_space_index);
+        let out = await call_normalization_model(truncated_text);
+        let tmp_norm_text = '';
+        Object.keys(out).forEach((key) => {
+            tmp_norm_text += out[key]['word'];
+            if (out[key]['entity'] != '0')
+            {
+                tmp_norm_text += out[key]['entity'];
+            }
+            tmp_norm_text += ' ';
+        })
+        result += tmp_norm_text.substring(0, tmp_norm_text.lastIndexOf('.'));
+        subtitles = tmp_norm_text.substring(tmp_norm_text.lastIndexOf('.')) + subtitles.substring(last_space_index);
+        console.log(subtitles.length);
+    }
+    while (subtitles.length > inputTextLength);
+    return result;
 }
 
-export function normalize_subtitles(subtitles) {
-    subtitles = clean_subtitles(subtitles);
-    query({"inputs": subtitles}).then((response) => {
-        let result = '';
-        for (let i = 0; i < response.length; i++) {
-            let curr_entity_group = response[i]['entity_group'];
-            let curr_word = response[i]['word'];
-            if (curr_entity_group.startsWith('Upper')) {
-                result += curr_word.charAt(0).toUpperCase() + curr_word.slice(1);
-            }
-            else if (curr_entity_group.startsWith('lower')){
-                result += curr_word.charAt(0).toLowerCase() + curr_word.slice(1);
-            }
-            if (!curr_entity_group.endsWith('_')) {
-                result += curr_entity_group.charAt(curr_entity_group.length - 1);
-            }
-            result += ' ';
-        }
-        return result;
-    }).catch((error) => {
-        console.error('Error:', error);
-    });
+function extractLastSpaceIndex(text) {
+    if (text.length <= inputTextLength) {
+        return text.length;
+    } else {
+        const trimmedText = text.substring(0, inputTextLength);
+        const lastSpaceIndex = trimmedText.lastIndexOf(' ');
+        return lastSpaceIndex;
+    }
 }
 
 export function clean_subtitles(subtitles){
