5dc1cf9bebb3b5414438ddbcb855c787bd878442
eugenos-programos <elena.samchuk.73@mail.ru>
2024-02-14 14:20:16 +0300

    feat: replace onnx model on inference API requests

--- src/transformer.js
+++ src/transformer.js
@@ -1,9 +1,18 @@
-import { pipeline } from '@xenova/transformers';
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/oliverguhr/fullstop-punctuation-multilang-large",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
 
-
-export async function call_normalization_model(subtitles)
+export async function callPunctuationModel(subtitles)
 {
-    let pipe = await pipeline("token-classification", "ldenoue/fullstop-punctuation-multilang-large");
-    let out = await pipe(subtitles);
+    let out = await query({"inputs": subtitles});
     return out;
 }
