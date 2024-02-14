d20255ddfc2bf3c61980a798554f9aa544d4a211
eugenos-programos <elena.samchuk.73@mail.ru>
2024-02-14 14:23:04 +0300

    fix: separating subtitles on chunks + uppercasing after dot.

--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,49 @@
-import { call_normalization_model } from "./transformer.js";
+import { callPunctuationModel } from "./transformer.js";
 
 
 const inputTextLength = 1500;
 
-export async function normalize_subtitles(subtitles) {
+export async function normalize_subtitles(subtitles) 
+{
     subtitles = clean_subtitles(subtitles);
     let result = '';
     do
     {
         let last_space_index = extractLastSpaceIndex(subtitles);
         let truncated_text = subtitles.substring(0, last_space_index);
-        let out = await call_normalization_model(truncated_text);
+        let out = await callPunctuationModel(truncated_text);
         let tmp_norm_text = '';
         Object.keys(out).forEach((key) => {
             tmp_norm_text += out[key]['word'];
-            if (out[key]['entity'] != '0')
+            if (out[key]['entity_group'] != '0')
             {
-                tmp_norm_text += out[key]['entity'];
+                tmp_norm_text += out[key]['entity_group'];
             }
             tmp_norm_text += ' ';
         })
-        result += tmp_norm_text.substring(0, tmp_norm_text.lastIndexOf('.'));
-        subtitles = tmp_norm_text.substring(tmp_norm_text.lastIndexOf('.')) + subtitles.substring(last_space_index);
-        console.log(subtitles.length);
+        if (subtitles.length < inputTextLength)
+        {
+            result += tmp_norm_text;
+            break;
+        }
+        else
+        {
+            result += tmp_norm_text.substring(0, tmp_norm_text.lastIndexOf('.'));
+        }
+        subtitles = tmp_norm_text.substring(tmp_norm_text.lastIndexOf('.') + 1) + subtitles.substring(last_space_index);
     }
-    while (subtitles.length > inputTextLength);
-    return result;
+    while (true);
+    return cleanNormalizedText(result);
 }
 
-function extractLastSpaceIndex(text) {
+function cleanNormalizedText(text)
+{
+    text = capitalizeAfterDot(text);
+    return text;
+}
+
+function extractLastSpaceIndex(text) 
+{
     if (text.length <= inputTextLength) {
         return text.length;
     } else {
@@ -38,7 +53,14 @@ function extractLastSpaceIndex(text) {
     }
 }
 
-export function clean_subtitles(subtitles){
+function capitalizeAfterDot(text) {
+    return text.replace(/\.(\s*[a-z])/g, function(match) {
+        return match.toUpperCase();
+    });
+}
+
+function clean_subtitles(subtitles)
+{
     let cleaned_subtitles = subtitles.toString().replace(/\([^)]*\)/g, '');
     return cleaned_subtitles;
 }
